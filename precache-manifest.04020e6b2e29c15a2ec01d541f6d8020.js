b6bdfb78437ac1987bc62605e1c175ef15889828
Straider71 <straider71@gmail.com>
2019-08-13 17:12:20 +0430

    Updates

---
+++ precache-manifest.04020e6b2e29c15a2ec01d541f6d8020.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8a20c7adf2827cf452b4d0c2b5a22f",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "be85a5e3dd1e27a39e48",
+    "url": "/monster/static/css/main.edf6c543.chunk.css"
+  },
+  {
+    "revision": "aa48f56478f6a0e31d27",
+    "url": "/monster/static/js/2.234e5899.chunk.js"
+  },
+  {
+    "revision": "be85a5e3dd1e27a39e48",
+    "url": "/monster/static/js/main.6524af0d.chunk.js"
+  },
+  {
+    "revision": "3980fef02df839b01168",
+    "url": "/monster/static/js/runtime~main.44aa05d7.js"
+  }
+]);
